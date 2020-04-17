591a4b599fd17ddf641ab2b4e9739e0a6fff0e9b
Alon Cohen-Magen <alloncm@gmail.com>
2020-04-18 01:44:00 +0300

    added rect and wire frame mode

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include<iostream>
+#include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
@@ -9,22 +9,29 @@ void ProcessIntput(GLFWwindow *window)
     {
         glfwSetWindowShouldClose(window, GLFW_TRUE);
     }
+    if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
+    {
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    }
+    if (glfwGetKey(window, GLFW_KEY_ENTER) == GLFW_PRESS)
+    {
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    }
 }
 
 const char *fragmentShaderSource = "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "void main()\n"
-    "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-    "}\0";
+                                   "out vec4 FragColor;\n"
+                                   "void main()\n"
+                                   "{\n"
+                                   "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                   "}\0";
 
 const char *vertexShaderSource = "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}\0";
-
+                                 "layout (location = 0) in vec3 aPos;\n"
+                                 "void main()\n"
+                                 "{\n"
+                                 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                 "}\0";
 
 int main()
 {
@@ -62,7 +69,8 @@ int main()
     {
         char logInfo[512];
         glGetShaderInfoLog(vertexShaderId, 512, NULL, logInfo);
-        std::cout<<"ERROR compiling vertex shader\n"<<logInfo<<std::endl;
+        std::cout << "ERROR compiling vertex shader\n"
+                  << logInfo << std::endl;
         return 1;
     }
 
@@ -72,11 +80,12 @@ int main()
     glCompileShader(fragmentShaderId);
 
     glGetShaderiv(fragmentShaderId, GL_COMPILE_STATUS, &sucess);
-    if(!sucess)
+    if (!sucess)
     {
         char logInfo[512];
         glGetShaderInfoLog(vertexShaderId, 512, NULL, logInfo);
-        std::cout<<"ERROR compiling fragment shader\n"<<logInfo<<std::endl;
+        std::cout << "ERROR compiling fragment shader\n"
+                  << logInfo << std::endl;
         return 1;
     }
 
@@ -87,36 +96,45 @@ int main()
     glLinkProgram(shaderProgram);
 
     glGetProgramiv(shaderProgram, GL_LINK_STATUS, &sucess);
-    if(!sucess)
+    if (!sucess)
     {
         char logInfo[512];
         glGetProgramInfoLog(shaderProgram, 512, NULL, logInfo);
-        std::cout<<"ERROR linking shaders program shader\n"<<logInfo<<std::endl;
+        std::cout << "ERROR linking shaders program shader\n"
+                  << logInfo << std::endl;
         return 1;
     }
 
     glDeleteShader(vertexShaderId);
     glDeleteShader(fragmentShaderId);
 
-
     float vertices[] = {
+        -0.5f, 0.5f, 0.0f,
         -0.5f, -0.5f, 0.0f,
         0.5f, -0.5f, 0.0f,
-        0.0f, 0.5f, 0.0f};
-
+        0.5f, 0.5f, 0.0f};
+    unsigned int indicies[] = {
+        0, 1, 2,
+        0, 2, 3};
     unsigned int vertexArrayObj;
     glGenVertexArrays(1, &vertexArrayObj);
     glBindVertexArray(vertexArrayObj);
-    unsigned int vertexBufferObj;
-    glGenBuffers(1,&vertexBufferObj);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObj);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices),vertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0,3,GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+    unsigned int elementBufferObj;
+    glGenBuffers(1, &elementBufferObj);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementBufferObj);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicies), indicies, GL_STATIC_DRAW);
+
+    unsigned int vertexBufferObj;
+    glGenBuffers(1, &vertexBufferObj);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObj);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
     glEnableVertexAttribArray(0);
 
-
-
+    glUseProgram(shaderProgram);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     while (!glfwWindowShouldClose(window))
     {
         ProcessIntput(window);
@@ -124,9 +142,7 @@ int main()
         //rendering
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-        glUseProgram(shaderProgram);
-        glBindVertexArray(vertexArrayObj);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
