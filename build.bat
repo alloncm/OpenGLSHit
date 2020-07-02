d48bd1469075c9c58afc0649df6a216fdea80a7f
alloncm <alloncm@gmail.com>
2020-07-02 21:01:13 +0300

    another fix

--- build.bat
+++ build.bat
@@ -1,2 +1,4 @@
+mkdir build
+cd build
 cmake .. -G "Visual Studio 16"
 cmake --build . --config Release
--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
+mkdir build
+cd build
 cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
 cmake --build .
